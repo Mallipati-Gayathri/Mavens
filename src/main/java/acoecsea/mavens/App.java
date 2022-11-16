664571677c7377abc17effbbbdd744025823ad9b
pc <pc@LAPTOP-K6PNLRFQ>
2022-11-16 23:15:37 +0530

    commit message

---
+++ src/main/java/acoecsea/mavens/App.java
@@ -0,0 +1,18 @@
+package acoecsea.mavens;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to devops lab" );
+    }
+    public int sum(int a ,int b) {
+    	return (a+b);
+    	
+    }
+    
+}
